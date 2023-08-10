f5ad2940ecc148afa27009ff73fac924d5de3529
Siham Elhilali <selhilal@e2r6p9.1337.ma>
2023-08-10 18:06:15 +0100

    push

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
-
-def add(a, b):
-    print("Adding %d + %d = %d" % (a, b, a + b))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
