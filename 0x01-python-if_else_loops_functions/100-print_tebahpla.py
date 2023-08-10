8ad0a569dc8bef3b7db937d6bd3c5f0d83cf9841
Siham Elhilali <selhilal@e2r6p9.1337.ma>
2023-08-10 17:49:34 +0100

    push

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,5 +4,5 @@ start = ord('z')
 end = ord('a')
 for i in range(start, end - 1, -1):
     if i % 2 == 1:
-        print("{:c}".format(i - 32), end="")
+        i -= 32
     print("{:c}".format(i), end="")
