64c154eed7f5b955ea4a83c4ddda03f06c0449c9
Siham Elhilali <selhilal@e2r6p9.1337.ma>
2023-08-10 16:27:31 +0100

    push

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-for a in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(a), end="")
+start = ord('a')
+end = ord('z')
+for a in range(start, end + 1):
+    print("{:c}".format(a), end=" ")
