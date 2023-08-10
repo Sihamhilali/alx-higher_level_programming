34694cbb00e522cc1e94c07133c854f7d247a58d
Siham Elhilali <selhilal@e2r6p9.1337.ma>
2023-08-10 15:32:57 +0100

    push

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *current, *check;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	current = list;
+	check = current->next;
+
+	while (current != NULL && check->next != NULL
+			&& check->next->next != NULL)
+	{
+		if (current == check)
+			return (1);
+		current = current->next;
+		check = check->next->next;
+	}
+	return (0);
+}
