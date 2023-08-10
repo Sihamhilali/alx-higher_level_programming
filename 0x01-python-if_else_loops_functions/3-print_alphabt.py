7a6666ecc1b93c7ebe44854bf394796d9369b52a
Siham Elhilali <selhilal@e2r6p9.1337.ma>
2023-08-10 16:53:12 +0100

    push

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,6 @@
 
 start = ord('a')
 end = ord('z')
-for a in  range(start, end + 1): 
+for a in  range(start, end + 1):
     if chr(a) != 'e' and chr(a) != 'q':
         print('{:s}'.format(chr(a)), end='')
